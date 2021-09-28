1c028bd094c013ff9f09787671a771d40a2b7388
Felipe Giraldo <felipe-giraldo@hotmail.es>
2021-09-28 15:36:05 -0500

    empezando a trabajar con typeScript

---
+++ ejercicios/scope.js
@@ -0,0 +1,28 @@
+//Global Scope
+    // var message = "hello world";
+    // var $ = function (message){
+    //   console.log("Say: "+ message);
+    // }
+    //Function Scope
+    // function printNumbers(){
+    //   var i;
+    //   for(i = 0; i < 10; i++){
+    //     function eventuallyPrintNumbers(n){
+    //       setTimeout(function () {
+    //         console.log(n);
+    //       }, 100);
+    //     }
+    //     eventuallyPrintNumbers(i);
+    //   }
+    // }
+    // printNumbers();
+    //Black Scope
+    // function printNumbers2() {      
+    //   for (let i = 0; i < 10; i++) {
+    //     setTimeout(function () {
+    //       console.log(i);
+    //     }, 100);
+    //   }
+    // }
+    // printNumbers2();
+    //Module Scope
